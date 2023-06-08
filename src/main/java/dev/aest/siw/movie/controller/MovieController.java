253ca24d43cf4505e9c2c89cea1ff0f2b146d4e1
Davide Pierotti <d.pierotti@live.it>
2023-06-08 18:12:32 +0200

    Add basic movies controller

--- src/main/java/dev/aest/siw/movie/controller/MovieController.java
+++ src/main/java/dev/aest/siw/movie/controller/MovieController.java
@@ -2,6 +2,8 @@ package dev.aest.siw.movie.controller;
 
 import dev.aest.siw.movie.service.MovieService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MovieController
@@ -10,5 +12,10 @@ public class MovieController
 
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
+    }
+
+    @GetMapping("/movies")
+    public String movieHome(Model model){
+        return "movies/home";
     }
 }
