dee201459fd87b16f5e2aca8d485099d9bfc4b1f
Davide Pierotti <d.pierotti@live.it>
2023-06-18 01:36:24 +0200

    Add current URL as thymeleaf model attribute

--- src/main/java/dev/aest/siw/movie/controller/GlobalController.java
+++ src/main/java/dev/aest/siw/movie/controller/GlobalController.java
@@ -2,12 +2,18 @@ package dev.aest.siw.movie.controller;
 
 import dev.aest.siw.movie.SiwMovie;
 import dev.aest.siw.movie.service.CredentialsService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class GlobalController
 {
+    @ModelAttribute("requestUrl")
+    public String addCurrentAddress(HttpServletRequest request){
+        return request.getServletPath();
+    }
+
     @ModelAttribute("project")
     public String addProjectName(){
         return SiwMovie.PROJECT_NAME;
