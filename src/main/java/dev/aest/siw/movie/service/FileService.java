39734966ad3893178dfc5b1b89b064bc9ed9e633
Davide Pierotti <d.pierotti@live.it>
2023-06-25 20:48:25 +0200

    Return string instead of path when saving an image

--- src/main/java/dev/aest/siw/movie/service/FileService.java
+++ src/main/java/dev/aest/siw/movie/service/FileService.java
@@ -3,7 +3,6 @@ package dev.aest.siw.movie.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -12,8 +11,6 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
-import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 public abstract class FileService implements IFileService
@@ -30,11 +27,11 @@ public abstract class FileService implements IFileService
     }
 
     @Override
-    public Path save(MultipartFile file) {
-        Path filepath = this.root.resolve(UUID.randomUUID() + "-" + file.getOriginalFilename());
+    public String save(MultipartFile file) {
+        Path filepath = this.root.resolve(System.currentTimeMillis() + "-" + file.getOriginalFilename());
         try {
             Files.copy(file.getInputStream(), filepath);
-            return Paths.get("").relativize(filepath);
+            return filepath.toString().substring(1);
         } catch (FileAlreadyExistsException e) {
             throw new RuntimeException("A file of that name already exists. => " + filepath);
         } catch (Exception e) {
--- src/main/java/dev/aest/siw/movie/service/IFileService.java
+++ src/main/java/dev/aest/siw/movie/service/IFileService.java
@@ -3,14 +3,11 @@ package dev.aest.siw.movie.service;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Path;
-import java.util.stream.Stream;
-
 public interface IFileService
 {
     void init();
 
-    Path save(MultipartFile file);
+    String save(MultipartFile file);
 
     Resource load(String filename);
 }
