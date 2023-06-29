034873798b46e1b407b5cc56a1fb8463785d59eb
Davide Pierotti <d.pierotti@live.it>
2023-06-29 03:51:06 +0200

    Throw exception when login username is not present in database
    Changed the logic in the method loadUserByUsername, in the CredentialsService class. Instead of returning null when a user is not found, the method now throws UsernameNotFoundException. This is done to adhere to the interface contract..

--- src/main/java/dev/aest/siw/movie/service/CredentialsService.java
+++ src/main/java/dev/aest/siw/movie/service/CredentialsService.java
@@ -3,15 +3,11 @@ package dev.aest.siw.movie.service;
 import dev.aest.siw.movie.model.Credentials;
 import dev.aest.siw.movie.repository.CredentialsRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -45,6 +41,6 @@ public class CredentialsService implements UserDetailsService
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return credentialsRepository.findByUsername(username).orElse(null);
+        return credentialsRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
     }
 }
